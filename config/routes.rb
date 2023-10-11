21b987843945c89d2a4c62f24ed98428c508f1d0
Nathan Hartojo <nathanhartojo@gmail.com>
2023-10-11 03:02:30 +0000

    testing edit routes

--- config/routes.rb
+++ config/routes.rb
@@ -20,7 +20,7 @@ Rails.application.routes.draw do
   resources :posts do
     resources :comments
   end
-  
+
   resources :items do
     member do
       get :delete
@@ -34,5 +34,3 @@ Rails.application.routes.draw do
   get 'member-items', to: 'items#member_items'
   get 'transactions', to: 'transactions#index'
 end
-
-
