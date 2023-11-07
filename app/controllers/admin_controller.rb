3e0d06e5065d6c2a865be402296507adf2c29f0d
Nathan Hartojo <nathanhartojo@gmail.com>
2023-11-07 18:42:51 +0000

    admin can create users

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -10,6 +10,19 @@ class AdminController < ApplicationController
     @user = User.new
   end
 
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      redirect_to admin_users_path, notice: "User created successfully!"
+    else
+      redirect_to admin_users_path, alert: "User creation failed!"
+    end
+  end
+
+  def user_params
+    params.require(:user).permit(:fname, :lname, :email, :role, :password, :password_confirmation, :callsign)
+  end
+
   def transactions
     @transactions = Transaction.all
   end
