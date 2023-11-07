b8126f4804d66f23aec551c825de46ecbce6f77c
Nathan Hartojo <nathanhartojo@gmail.com>
2023-11-07 18:13:52 +0000

    refactoring admin controller

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -4,17 +4,18 @@ class AdminController < ApplicationController
 
   def index
   end
-  
+
   def users
     @users = User.all
+    @user = User.new
   end
-  
+
   def transactions
     @transactions = Transaction.all
   end
-  
+
   private
-  
+
   def ensure_admin
     unless current_user && current_user.admin?
       redirect_to root_path, alert: "Access denied!"
