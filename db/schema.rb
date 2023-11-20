9510928bcc610c5a30bf6c412a9838aa36530784
William Kang <wkang2020@gmail.com>
2023-11-20 00:01:54 -0600

    fixed import csv function

--- db/schema.rb
+++ db/schema.rb
@@ -1,5 +1,3 @@
-# frozen_string_literal: true
-
 # This file is auto-generated from the current state of the database. Instead
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
@@ -11,95 +9,92 @@
 # migrations use external dependencies or application code.
 #
 # It's strongly recommended that you check this file into your version control system.
- 
-ActiveRecord::Schema[7.0].define(version: 20_231_119_212_844) do
+
+ActiveRecord::Schema[7.0].define(version: 2023_11_19_212844) do
   # These are extensions that must be enabled in order to support this database
-  enable_extension 'plpgsql'
+  enable_extension "plpgsql"
 
-  create_table 'active_storage_attachments', force: :cascade do |t|
-    t.string 'name', null: false
-    t.string 'record_type', null: false
-    t.bigint 'record_id', null: false
-    t.bigint 'blob_id', null: false
-    t.datetime 'created_at', null: false
-    t.index ['blob_id'], name: 'index_active_storage_attachments_on_blob_id'
-    t.index %w[record_type record_id name blob_id], name: 'index_active_storage_attachments_uniqueness',
-                                                    unique: true
+  create_table "active_storage_attachments", force: :cascade do |t|
+    t.string "name", null: false
+    t.string "record_type", null: false
+    t.bigint "record_id", null: false
+    t.bigint "blob_id", null: false
+    t.datetime "created_at", null: false
+    t.index ["blob_id"], name: "index_active_storage_attachments_on_blob_id"
+    t.index ["record_type", "record_id", "name", "blob_id"], name: "index_active_storage_attachments_uniqueness", unique: true
   end
 
-  create_table 'active_storage_blobs', force: :cascade do |t|
-    t.string 'key', null: false
-    t.string 'filename', null: false
-    t.string 'content_type'
-    t.text 'metadata'
-    t.string 'service_name', null: false
-    t.bigint 'byte_size', null: false
-    t.string 'checksum'
-    t.datetime 'created_at', null: false
-    t.index ['key'], name: 'index_active_storage_blobs_on_key', unique: true
+  create_table "active_storage_blobs", force: :cascade do |t|
+    t.string "key", null: false
+    t.string "filename", null: false
+    t.string "content_type"
+    t.text "metadata"
+    t.string "service_name", null: false
+    t.bigint "byte_size", null: false
+    t.string "checksum"
+    t.datetime "created_at", null: false
+    t.index ["key"], name: "index_active_storage_blobs_on_key", unique: true
   end
 
-  create_table 'active_storage_variant_records', force: :cascade do |t|
-    t.bigint 'blob_id', null: false
-    t.string 'variation_digest', null: false
-    t.index %w[blob_id variation_digest], name: 'index_active_storage_variant_records_uniqueness', unique: true
+  create_table "active_storage_variant_records", force: :cascade do |t|
+    t.bigint "blob_id", null: false
+    t.string "variation_digest", null: false
+    t.index ["blob_id", "variation_digest"], name: "index_active_storage_variant_records_uniqueness", unique: true
   end
 
-  create_table 'consumables', force: :cascade do |t|
-    t.string 'name'
-    t.text 'description'
-    t.integer 'quantity_remaining'
-    t.datetime 'created_at', null: false
-    t.datetime 'updated_at', null: false
- 
- 
+  create_table "consumables", force: :cascade do |t|
+    t.string "name"
+    t.text "description"
+    t.integer "quantity_remaining"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
-  create_table 'items', force: :cascade do |t|
-    t.string 'name'
-    t.text 'description'
-    t.string 'serial_number'
-    t.boolean 'available'
-    t.datetime 'created_at', null: false
-    t.datetime 'updated_at', null: false
+  create_table "items", force: :cascade do |t|
+    t.string "name"
+    t.text "description"
+    t.string "serial_number"
+    t.boolean "available"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
-  create_table 'maintenance_items', force: :cascade do |t|
-    t.string 'item_name'
-    t.string 'description'
-    t.integer 'total_amount'
-    t.integer 'available_amount'
-    t.datetime 'created_at', null: false
-    t.datetime 'updated_at', null: false
+  create_table "maintenance_items", force: :cascade do |t|
+    t.string "item_name"
+    t.string "description"
+    t.integer "total_amount"
+    t.integer "available_amount"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
-  create_table 'transactions', force: :cascade do |t|
-    t.string 'email'
-    t.string 'serial_number'
-    t.boolean 'approved'
-    t.datetime 'created_at', default: -> { 'CURRENT_TIMESTAMP' }, null: false
-    t.datetime 'updated_at', default: -> { 'CURRENT_TIMESTAMP' }, null: false
+  create_table "transactions", force: :cascade do |t|
+    t.string "email"
+    t.string "serial_number"
+    t.boolean "approved"
+    t.datetime "created_at", default: -> { "CURRENT_TIMESTAMP" }, null: false
+    t.datetime "updated_at", default: -> { "CURRENT_TIMESTAMP" }, null: false
   end
 
-  create_table 'users', force: :cascade do |t|
-    t.string 'email', default: '', null: false
-    t.string 'encrypted_password', default: '', null: false
-    t.string 'reset_password_token'
-    t.datetime 'reset_password_sent_at'
-    t.datetime 'remember_created_at'
-    t.datetime 'created_at', null: false
-    t.datetime 'updated_at', null: false
-    t.string 'fname'
-    t.string 'lname'
-    t.string 'role'
-    t.string 'provider'
-    t.string 'uid'
-    t.jsonb 'google_data'
-    t.string 'callsign'
-    t.index ['email'], name: 'index_users_on_email', unique: true
-    t.index ['reset_password_token'], name: 'index_users_on_reset_password_token', unique: true
+  create_table "users", force: :cascade do |t|
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.string "fname"
+    t.string "lname"
+    t.string "role"
+    t.string "provider"
+    t.string "uid"
+    t.jsonb "google_data"
+    t.string "callsign"
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
-  add_foreign_key 'active_storage_attachments', 'active_storage_blobs', column: 'blob_id'
-  add_foreign_key 'active_storage_variant_records', 'active_storage_blobs', column: 'blob_id'
+  add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
+  add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
 end
